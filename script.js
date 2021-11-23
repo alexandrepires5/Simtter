b7dde45d9bbb4ef79cf8182c6ca41b2de9646062
Alexandre Pires <apthoughts@protonmail.com>
2021-11-23 23:57:50 +0100

    Add events to modal post. Add events for page display after post.

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const newsFeedPage = document.querySelector('.feeds-page');
 const loginModel = document.querySelector('.login-model');
 const loginModelX = document.querySelector('.login-model i');
 const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header i');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
 //---------------------------------------------
 //---------------------------------------------
 
@@ -49,3 +56,36 @@ loginFormBtn.addEventListener('click', () => {
         loginModel.style.display = 'block';
     }
 }); 
+
+//News feed page
+// Post modal
+postBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalWrapper.classList.add('modal-wrapper-display');
+});
+
+const changeOpacity = (x) => {
+    modalPostBtn.style.opacity = x;
+    modalFooterPlus.style.opacity = x;
+};
+
+postModalX.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalWrapper.classList.remove('modal-wrapper-display');
+    if(modalInput.value !== ''){
+        modalInput.value = '';
+        changeOpacity(.5);
+    }
+});
+
+modalInput.addEventListener('keypress', (e) => {
+    if(e.target.value !== ""){
+        changeOpacity(1);
+    }
+});
+
+modalInput.addEventListener('blur', (e) => {
+    if(e.target.value === ""){
+        changeOpacity(.5);
+    }
+});
